5b02a995953897dd7d3bfd130bfb09e31c093d2b
Amanda Westerlind <amanda.westerlind@live.se>
2025-10-06 00:14:56 +0200

    Deleted function that is not in use atm.

--- Labb2_DungeonCrawler/Position.cs
+++ Labb2_DungeonCrawler/Position.cs
@@ -8,24 +8,7 @@ namespace Labb2_DungeonCrawler
 {
     class Position
     {
-        public static void IsEnemyInsideLevel(Enemy enemy, IReadOnlyList<LevelElement> Elements, int oldx, int oldy)
-        {
-            ///TODO: måste även kolla koordinater mot spelaren.
-            foreach (var element in Elements)
-            {
-                
-                if (enemy.x == element.x && enemy.y == element.y)
-                {
-                    enemy.x = oldx;
-                    enemy.y = oldy;
-                    break;
-
-                    
-                }
-            }
-        }
-
-        public static void IsPlayerInsideLevel(Player p, IReadOnlyList<LevelElement> Elements, int oldx, int oldy)
+              public static void IsPlayerInsideLevel(Player p, IReadOnlyList<LevelElement> Elements, int oldx, int oldy)
         {
             foreach (var element in Elements)
             {
